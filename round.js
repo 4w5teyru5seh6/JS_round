38dab88baeaaf0b63db840b0b566bd5ac530cd8b
4w5teyru5seh6 <105905275+4w5teyru5seh6@users.noreply.github.com>
2022-12-28 23:42:56 +0900

    Update round.js

--- round.js
+++ round.js
@@ -1,41 +1,38 @@
 function round(num,digit=0){
+  let fraction = 1;
   if(digit > 0){
-    var fraction = 10 ** digit;
+    fraction = 10 ** digit;
   }else if(digit < 0){
     digit = Math.abs(digit);
-    var fraction = 0.1 ** digit;
-  }else{
-    fraction = 1;
+    fraction = 0.1 ** digit;
   }
 
-  var result = Math.round(num * fraction) / fraction;
+  let result = Math.round(num * fraction) / fraction;
   return result;
 }
 
 function roundup(num,digit=0){
+  let fraction = 1;
   if(digit > 0){
-    var fraction = 10 ** digit;
+    fraction = 10 ** digit;
   }else if(digit < 0){
     digit = Math.abs(digit);
-    var fraction = 0.1 ** digit;
-  }else{
-    fraction = 1;
+    fraction = 0.1 ** digit;
   }
 
-  var result = Math.ceil(num * fraction) / fraction;
+  let result = Math.ceil(num * fraction) / fraction;
   return result;
 }
 
 function rounddown(num,digit=0){
+  let fraction = 1;
   if(digit > 0){
-    var fraction = 10 ** digit;
+    fraction = 10 ** digit;
   }else if(digit < 0){
     digit = Math.abs(digit);
-    var fraction = 0.1 ** digit;
-  }else{
-    fraction = 1;
+    fraction = 0.1 ** digit;
   }
 
-  var result = Math.floor(num * fraction) / fraction;
+  let result = Math.floor(num * fraction) / fraction;
   return result;
 }
